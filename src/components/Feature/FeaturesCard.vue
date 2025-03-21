ba9d60e0fb1772e1cdce9751a726f5cf7fd78f10
RayLonscholar <f82621672@gmail.com>
2025-03-21 15:02:28 +0000

    feat: add feature cards

---
+++ src/components/Feature/FeaturesCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="text-center mt-[100px] bg-white shadow-md shadow-gray-500 rounded-[4px] w-[320px] self-center p-5"
+  >
+    <slot>
+      <dev class="text-[40px] mb-4">
+        <slot name="icon"></slot>
+      </dev>
+      <p class="text-[20px] mb-2.5">
+        <slot name="title"></slot>
+      </p>
+      <p class="text-[16px]">
+        <slot name="text"></slot>
+      </p>
+    </slot>
+  </div>
+</template>
---
+++ src/components/Feature/FeaturesCardList.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="grid gap-1 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 justify-around flex-wrap m-[0px 16px 16px 16px] place-items-center place-content-center"
+  >
+    <slot></slot>
+  </div>
+</template>
