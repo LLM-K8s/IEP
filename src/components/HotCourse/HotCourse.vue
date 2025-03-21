3cce1bf0ee408d70424659332f5541e7742a4f62
RayLonscholar <f82621672@gmail.com>
2025-03-21 17:14:28 +0000

    feat: hot-course list

---
+++ src/components/HotCourse/HotCourse.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="border-1 border-[#ddd] border-solid rounded-[8px] overflow-hidden"
+  >
+    <div class="h-[150px] bg-[#eee]">
+      <img alt="課程圖片" />
+    </div>
+    <div class="p-[15px]">
+      <p class="text-[20px]">課程名稱</p>
+      <div class="flex justify-between text-[#666] text-[16px] mt-2 mb-4">
+        <span>講師: </span>
+        <span>5.0 ⭐</span>
+      </div>
+      <a href="#" class="bg-[#3498db] text-white rounded-lg p-2">查看詳情</a>
+    </div>
+  </div>
+</template>
---
+++ src/components/HotCourse/HotCourseLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div class="bg-white shadow-gray-500 rounded-[8px] mx-[64px]">
+      <p class="text-[24px] p-4 mt-[120px] font-bold">熱門課程</p>
+      <div class="grid gap-1 grid-cols-3 mx-4">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
