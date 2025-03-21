801c8226d18aa5710b4175480cbcfe918fc03906
RayLonscholar <f82621672@gmail.com>
2025-03-21 17:12:57 +0000

    feat: add feature title

--- src/components/Feature/FeaturesCard.vue
+++ src/components/Feature/FeaturesCard.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="text-center mt-[100px] bg-white shadow-md shadow-gray-500 rounded-[4px] w-[320px] self-center p-5"
+    class="text-center mt-[30px] bg-white shadow-md shadow-gray-500 rounded-[8px] w-[320px] self-center p-5"
   >
     <slot>
       <dev class="text-[40px] mb-4">
         <slot name="icon"></slot>
       </dev>
-      <p class="text-[20px] mb-2.5">
+      <p class="text-[20px] font-bold mb-2.5">
         <slot name="title"></slot>
       </p>
       <p class="text-[16px]">
--- src/components/Feature/FeaturesCardList.vue
+++ src/components/Feature/FeaturesCardList.vue
@@ -1,7 +1,10 @@
 <template>
-  <div
-    class="grid gap-1 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 justify-around flex-wrap m-[0px 16px 16px 16px] place-items-center place-content-center"
-  >
-    <slot></slot>
+  <div>
+    <p class="text-[24px] text-center mt-[120px] font-bold">平台特色</p>
+    <div
+      class="grid gap-1 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 justify-around flex-wrap mx-[16px] place-items-center place-content-center"
+    >
+      <slot></slot>
+    </div>
   </div>
 </template>
