032c80524e61438fd46673ca87a0b208c957a6f8
RayLonscholar <f82621672@gmail.com>
2025-03-24 12:55:23 +0000

    style: add font bold

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -5,20 +5,22 @@
     >
       <div class="bg-neutral-100/60 rounded-2xl p-4 m-4">
         <h1 class="text-black text-4xl font-bold mb-4">整合式教學平台</h1>
-        <p class="text-black text-[16px]">
+        <p class="text-black text-[16px] font-bold">
           探索豐富的課程，隨時隨地學習新知識，提升自己的能力。我們提供最優質的線上教學體驗，讓學習更有效率、更有樂趣。
         </p>
+        <div class="flex justify-between mt-4 mx-[30%]">
+          <a
+            href="#features"
+            class="bg-gray-800 text-gray-300 text-[16px] hover:text-white hover:bg-gray-600 rounded-lg p-2"
+            >平台特色</a
+          >
+          <a
+            href="#hotcourse"
+            class="bg-gray-800 text-gray-300 text-[16px] hover:text-white hover:bg-gray-600 rounded-lg p-2"
+            >熱門課程</a
+          >
+        </div>
       </div>
-      <a
-        href="#features"
-        class="bg-gray-800 text-gray-300 text-[16px] hover:text-white hover:bg-gray-600 rounded-lg p-2"
-        >平台特色</a
-      >
-      <a
-        href="#hotcourse"
-        class="bg-gray-800 text-gray-300 text-[16px] hover:text-white hover:bg-gray-600 rounded-lg p-2"
-        >熱門課程</a
-      >
     </div>
   </section>
 </template>
