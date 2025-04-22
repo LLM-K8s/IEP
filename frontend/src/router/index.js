3ebb211669e94ba363213cbc7b27b14f02646e7f
RayLonscholar <f82621672@gmail.com>
2025-04-22 14:27:25 +0000

    feat(router): add routes for MyCourse and Class views

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,8 @@ import { createWebHistory, createRouter } from "vue-router";
 import Home from "../views/Home.vue";
 import SelectCourse from "../views/SelectCourse.vue";
 import CreateCourse from "../views/CreateCourse.vue";
+import MyCourse from "../views/MyCourse.vue";
+import Class from "../views/Class.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -10,6 +12,8 @@ const router = createRouter({
     { path: "/", name: "Home", component: Home },
     { path: "/CreateCourse", name: "CreateCourse", component: CreateCourse },
     { path: "/SelectCourse", name: "SelectCourse", component: SelectCourse },
+    { path: "/MyCourse", name: "MyCourse", component: MyCourse },
+    { path: "/Class", name: "Class", component: Class },
   ],
 });
 
