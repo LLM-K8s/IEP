a0f0c3c9262a29fd86a61dbb68cd4f605534883c
RayLonscholar <f82621672@gmail.com>
2025-04-25 17:24:21 +0000

    style(main): standardize import quotes for consistency

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router/index.js";
-import { createPinia } from 'pinia';
+import { createPinia } from "pinia";
 
 const app = createApp(App);
 const pinia = createPinia();
