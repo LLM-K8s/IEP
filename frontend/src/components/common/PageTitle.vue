a9d42c8f1cdfaf421a73f4b67f23fdfc4081fb43
RayLonscholar <f82621672@gmail.com>
2025-05-13 15:42:38 +0000

    style(ui): Improve label and layout styling for common components
    - Updated label styles in `FileUpload.vue` and `Input.vue` to use larger font and bold weight
    - Refined layout and alignment in `PageTitle.vue`
      - Removed unnecessary flex wrapper
      - Adjusted back button spacing and overall title alignment

--- frontend/src/components/common/PageTitle.vue
+++ frontend/src/components/common/PageTitle.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="pt-20 w-[100%]">
-    <div class="flex items-center space-x-4">
-      <router-link
-        v-if="showBackButton"
-        :to="backRoute"
-        class="bg-[#3498db] hover:bg-[#2d83bc] text-white text-[24px] px-6 rounded-lg h-fit"
-      >
-        返回
-      </router-link>
-      <span class="text-[24px] mt-20 mb-[16px] font-bold h-fit">
-        {{ title }}
-      </span>
-    </div>
-    <hr class="border-2 border-gray-500 rounded-2xl mb-6" />
+    <router-link
+      v-if="showBackButton"
+      :to="backRoute"
+      class="bg-[#3498db] hover:bg-[#2d83bc] text-white text-[24px] self-center px-4 mr-4 rounded-lg h-fit"
+    >
+      返回
+    </router-link>
+    <span class="text-[24px] mt-20 mb-[16px] font-bold h-fit">
+      {{ title }}
+    </span>
+    <hr class="border-2 border-gray-500 rounded-2xl" />
   </div>
 </template>
 
